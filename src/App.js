462175c6bcebf4ba8dfe2d6d07a722f55c59397d
ridvankoseler <ridvankoseler@gmail.com>
2022-07-28 17:17:25 +0300

    LoremIpsum was created.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import text from "./helpers/data";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(true);
+
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if(number>=1){
+      setData(text.slice(0,(number)))
+      setShow(false)
+    }else{
+      setData([])
+      alert('Please enter a number greater than 1')
+    }
+  };
+
+  // console.log(text.length);
+  // console.log(data[0]);
+  // console.log(data.slice(0,number));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Ipsum">
+      <div className="container m-auto text-center mt-4 bg-secondary">
+      <div className="headerDiv ">
+        <h1 className="text-warning">TIRED OF BORING LOREM IPSUM?</h1>
+      </div>
+      <div className="formDiv mt-4">
+        <form action='paragraphs'>
+          <label className="fw-bold fs-2 " htmlFor=''>Paragraphs : </label>
+          <input className="inputDiv"
+            onChange={handleChange}
+            type='number'
+            name='number'
+            id='number'
+            value={number}
+            min='1'
+          />
+          <button className="submitDiv " onClick={handleClick} type='submit'>
+            GENERATE
+          </button>
+        </form>
+      </div>
+      <div className="articleDiv mt-5 bg-secondary col-xs-5">
+         {show ? (
+        <article>
+          <div className='list'>
+            <p>{text[1]}</p>
+          </div>
+        </article>
+      ) : (
+        <article>
+          {data.map((item, index) => {
+            return (
+              <div className="" key={index}>
+                <p>{item}</p>
+              </div>
+            );
+          })}
+        </article>
+      )}
+      </div>
     </div>
+    </div>
+    
   );
 }
 
